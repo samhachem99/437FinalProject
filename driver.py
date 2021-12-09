422be53fd8cbfca15038914e91144ce5f6425288
Sam Hachem <samhachem99@gmail.com>
2021-12-08 21:17:40 -0600

    created the driver file to accept command line arguments

---
+++ driver.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+text = "go forward 5 seconds, go left for 2 seconds, go right for 1 second, go backward for 10 cms"
+
+
+if __name__ == "__main__":
+    print(len(sys.argv))
+    print(sys.argv)
