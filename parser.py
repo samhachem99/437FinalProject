79a074ce52de7e09b72f3cb29321315936779bd0
stonov <60677967+stonov@users.noreply.github.com>
2021-12-09 23:34:29 -0600

    initial implementation of validator

--- parser.py
+++ parser.py
@@ -12,45 +12,76 @@ COMMANDS_CONSTS = [FROWARD, BACKWARD, LEFT, RIGHT, STOP, SPEED]
 # units acceptable
 SECONDS = "SECONDS"
 
-class Validation():
-    def __init__(self, text_list: list):
-        text = ""
-        text_list = text_list
-        
-    def __init__(self):
-        text = ""
-        text_list = []
-    
-    def __init__(self, text: str):
-        text = text
-        text_list = []
-        
-    def validate_direction(self, command: str):
-        pass
+# Language definitions
+# Language is all lowercase
+COMMAND_SEPARATOR = ","
+DIRECTIONS = ['left', 'right', 'backward', 'back', 'forward', 'straight']
+POWER = 'power'
 
-    def validate_duration(self, command: str):
-        pass
-
-    def validate_unit(self, command: str):
-        pass
-
-    def validate_command(self, command: str):
-        pass
-        
-    def validate(self):
+def doesListContainNumberString(myList):
+    for i in myList:
         try:
-            self.text_list.index(',')
+            converted = float(i)
+            return True
         except:
-            return 0
+            continue
+    return False
 
-        command_list = text.strip().lower().split(',')
-        for command in command_list:
-            validator_retval = self.validate_command(command)
-            if validator_retval != 0: # or somehting else
-                return 0
+class Validation():
+    def __init__(self, text):
+        self.commands = []
+
+        # Splits the dictation into separate commands
+        command_strings = text.lower().split(COMMAND_SEPARATOR)
+
+        # converts each command string into a list of keywords and stores it into
+        # self.commands
+        for command_string in command_strings:
+            self.commands.append(command_string.split())
+        print(self.commands)
+
+    # Returns whether the command contains a directional keyword
+    def isCommandDirectional(self, command: list):
+        return (bool(set(DIRECTIONS) & set(command)))
+    
+    # Returns whether the command contains a power keyword
+    def isCommandPower(self, command: list):
+        return (POWER in command)
+    
+    # Returns whether a power command is valid
+    # A power command is of the form:
+    #   SET POWER TO percentage
+    def validatePowerCommand(self, command: list):
+        return doesListContainNumberString(command)
+
+    # Returns whether a directional command is valid
+    # A directional command is of the form:
+    #   GO direction FOR time SECONDS
+    def validateDirectionalCommand(self, command: list):
+        return doesListContainNumberString(command)
+
+    # Returns whether a command is valid. Takes in a list of
+    # strings that denote a command
+    def isCommandValid(self, command: list):
+        if (self.isCommandDirectional(command)):
+            return self.validateDirectionalCommand(command)
+        elif (self.isCommandPower(command)):
+            return self.validatePowerCommand(command)
+        return False
+    
+    # Returns whether a dictation is valid
+    def isDictationValid(self):
+        for command in self.commands:
+            if (not self.isCommandValid(command)):
+                return False
+        return True
 
 if __name__ == "__main__":
+    # Gets the dictation from the shortcut
     text = sys.argv[1]
-    obj = Validation(text_list=text)
-    retval = obj.validate()
-    print(retval)
+    obj = Validation(text)
+    isDictationValid = obj.isDictationValid()
+    if (isDictationValid):
+        print("Dictation is valid!")
+    else:
+        print("Dictation is invalid.")
