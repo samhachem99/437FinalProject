e9b82d73e8668435674d5fe3a117c4bb3712f436
Sam Hachem <samhachem99@gmail.com>
2021-12-10 00:18:16 -0600

    added the driving capabilities to driver script

--- driver.py
+++ driver.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import time
-# import picar_4wd as fc
+import picar_4wd as fc
 
 # commands acceptable
 FROWARD = "FORWARD"
@@ -10,52 +10,53 @@ LEFT = "LEFT"
 RIGHT = "RIGHT"
 STOP = "STOP"
 POWER = "POWER"
-COMMANDS_CONSTS = [FROWARD, BACKWARD, LEFT, RIGHT, STOP, POWER]
+COMMANDS_CONSTS = [FROWARD, BACKWARD, LEFT, RIGHT]
 FILE_NAME = "commands.txt" # "/home/pi/sirimessages/picommands.txt"
-
-# units acceptable
-SECONDS = "SECONDS"
 
 power_val = 50
 fd = None
 
-# def process_data(data=""):
-#     global power_val
-
-#     if data != "":
-#         if data == FROWARD:
-#             fc.forward(power=power_val)
-#         elif data == BACKWARD:
-#             fc.backward(power=power_val)
-#         elif data == RIGHT:
-#             fc.turn_right(power=power_val)
-#         elif data == LEFT:
-#             fc.turn_left(power=power_val)
-#         elif data == POWER:
-#             power_val = min(100, power_val+10)
-#         elif data == STOP:
-#             fc.stop()
+def process_data(cmd: str, duration: float):
+    global power_val
+    
+    print("processing: {} for {}".format(cmd, duration))
+    if cmd in COMMANDS_CONSTS:
+        if cmd == FROWARD:
+            fc.forward(power=power_val)
+        elif cmd == BACKWARD:
+            fc.backward(power=power_val)
+        elif cmd == RIGHT:
+            fc.turn_right(power=power_val)
+        elif cmd == LEFT:
+            fc.turn_left(power=power_val)
+        time.sleep(duration)
+        fc.stop()
+    elif cmd == STOP:
+        fc.stop()
+    elif cmd == POWER:
+        power_val = duration*10
 
 def get_file_data():
     global fd
     
     if os.path.getsize(filename=FILE_NAME) == 0:
         return None
-    data = fd.readlines()
+    data = fd.read()
     fd.truncate(0)
     return data
        
 def run_driver():
     global fd
     
-    fd = open(FILE_NAME, "r+")
-    
     while True:
+        fd = open(FILE_NAME, "r+")
         data = get_file_data()
         if data != None:
-            print(data)
-        else:
-            print("file is empty")
+            commands_list = data.split('\n')
+            for command in commands_list:
+                cmd_duration = command.strip().split(' ')
+                process_data(cmd_duration[0], float(cmd_duration[1]))
+        fd.close()
         time.sleep(0.5)
 
 if __name__ == "__main__":
