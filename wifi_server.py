93e6cf29bba9b335abad97a534f8c960de38f58e
Sam Hachem <samhachem99@gmail.com>
2021-12-10 23:05:24 -0600

    removed print statements

--- wifi_server.py
+++ wifi_server.py
@@ -44,7 +44,6 @@ def ultra_handler():
     
     while running:
         ultra_status = fc.get_distance_at(0)
-        print("Ultrasonic Sensor reading: {}".format(ultra_status))
         if 20 <= ultra_status < 29:
             buzzer.beep_control(WARNING_ONE_INTERVAL)
         elif 10 <= ultra_status <= 20:
@@ -54,7 +53,6 @@ def ultra_handler():
         elif ultra_status < 4:
             buzzer.beep_control(buzzer.WARNING_FOUR_INTERVAL)
         elif ultra_status >= 30 or ultra_status == -2:
-            print("buzzer sleeping")
             buzzer.beep_control(WARNING_ONE_INTERVAL, active=0)
         sleep(0.5)
 
