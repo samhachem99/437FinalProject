18c50b545657a9fa7fb1479dee4dc53e95934668
stonov <60677967+stonov@users.noreply.github.com>
2021-12-12 15:01:11 -0600

    testing

--- driver.py
+++ driver.py
@@ -3,9 +3,9 @@ import wifi_server
 import car_controller
 
 def initialize_driver():
-    file_listener.setup_file_listener_thread(car_controller)
-    wifi_server.setup_wifi_thread(car_controller)
     car_controller.setup_threads()
+    file_listener.setup_file_listener_thread(car_controller)
+    # wifi_server.setup_wifi_thread(car_controller)
 
 if __name__ == "__main__":
     initialize_driver()
