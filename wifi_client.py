58bda7f64af3fdafe82b0140344f489eab408c32
Sam Hachem <samhachem99@gmail.com>
2021-12-12 21:11:25 -0600

    fixing wifi client

--- wifi_client.py
+++ wifi_client.py
@@ -1,5 +1,5 @@
 import socket
-import serial
+# import serial
 import time
 
 FROWARD = "FORWARD"
@@ -10,7 +10,7 @@ STOP = "STOP"
 SPEEDUP = "SPEEDUP"
 SPEEDDOWN = "SPEEDDOWN"
 
-CLIENT_HOST = "192.168.1.147" 
+CLIENT_HOST = "192.168.1.147"
 CLIENT_PORT = 65432
 BUFFER_SIZE = 4096
 
@@ -19,15 +19,15 @@ client = None
 def setup(host, port):
     global client
     
-    print("Connecting to Host {} and Port {}".format(host, port))
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((host,port))
-    print("connected....")
+    client.connect((CLIENT_HOST,CLIENT_PORT))
     
 def send_request(message: str):
     global client 
+    print("in sending mesage: {}".format(message))
     
     client.send(message.encode())
+    ataFromServer = client.recv(0);
     print("Message Sent.....")
     # data = str(client.recv(BUFFER_SIZE).decode("utf-8"))
     # print("Client sent back this: {}".format(data))
@@ -51,18 +51,27 @@ def process_request(message: str):
 if __name__ == "__main__":
     # setup(CLIENT_HOST, CLIENT_PORT)
     
-    arduino = serial.Serial(port='/dev/tty.usbmodem142301', baudrate=115200, timeout=.1)
-    
+    # arduino = serial.Serial(port='/dev/tty.usbmodem142301', baudrate=115200, timeout=.1)
+    print("Connecting to Host {} and Port {}".format(CLIENT_HOST, CLIENT_PORT))
     try: 
         while True:
-            value = arduino.readline()
-            print(value)
-            try: 
-                [x, y] = value.decode("utf-8").split(',')
-                print(x, y)
-                process_request()
-            except: 
-                print("not a point")
-            time.sleep(1)
-    except:
+            setup(CLIENT_HOST, CLIENT_PORT)
+            print("Connected...")
+            usr_val = input("what is your command?\n")
+            if usr_val == "qq":
+                break
+            process_request(usr_val)
+            # value = arduino.readline()
+            # print(value)
+            # try: 
+            #     [x, y] = value.decode("utf-8").split(',')
+            #     print(x, y)
+            #     process_request()
+            # except: 
+            #     print("not a point")
+            # time.sleep(1)
+            client.close()
+    except Exception as e:
+        print(e)
         client.close()
+    client.close()
